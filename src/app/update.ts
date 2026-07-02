b6165d6fadcce874b929733e54842b8b8d03f124
Ca-tt <damir96.lukyanenko@gmail.com>
2026-07-02 16:01:51 +0300

    Release v1.2.4

---
+++ src/app/update.ts
@@ -0,0 +1,26 @@
+import { updateElectronApp } from 'update-electron-app';
+
+const UPDATE_REPOSITORY = 'Ca-tt/codelevels-app';
+let updateCheckStarted = false;
+
+export const startAutoUpdateCheck = () => {
+  if (updateCheckStarted) {
+    return;
+  }
+
+  updateCheckStarted = true;
+
+  updateElectronApp({
+    repo: UPDATE_REPOSITORY,
+    updateInterval: '30 minutes',
+    notifyUser: false,
+  });
+};
+
+export const triggerManualUpdateCheck = () => {
+  updateElectronApp({
+    repo: UPDATE_REPOSITORY,
+    updateInterval: '120 minutes',
+    notifyUser: true,
+  });
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { app, BrowserWindow, Menu } from 'electron';
-import { updateElectronApp } from 'update-electron-app';
 
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
@@ -7,21 +6,7 @@ import started from 'electron-squirrel-startup';
 import { createMenu } from './browser/menu'
 import { enableShortcuts } from './browser/hotkeys'
 import { APP_CONFIG } from './config/app'
-
-let updateCheckStarted = false;
-
-const startAutoUpdateCheck = () => {
-  if (updateCheckStarted) {
-    return;
-  }
-
-  updateCheckStarted = true;
-  updateElectronApp({
-    repo: 'Ca-tt/codelevels-app',
-    updateInterval: '30 minutes',
-    notifyUser: false,
-  });
-};
+import { startAutoUpdateCheck } from './app/update'
 
 if (started) {
   app.quit();
@@ -42,17 +27,18 @@ const createWindow = () => {
   mainWindow.loadURL(APP_CONFIG.website.url);
 
   Menu.setApplicationMenu(Menu.buildFromTemplate(createMenu(mainWindow)))
-  // Menu.
+
   enableShortcuts(mainWindow)
-  startAutoUpdateCheck();
 
   if (APP_CONFIG.isConsoleOpened) {
     mainWindow.webContents.openDevTools();
   }
 };
 
-
-app.on('ready', createWindow);
+app.on('ready', () => {
+  startAutoUpdateCheck();
+  createWindow();
+});
 
 
 app.on('window-all-closed', () => {
@@ -66,12 +52,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-// and load the index.html of the app.
-// if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-//   mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-// } else {
-//   mainWindow.loadFile(
-//     path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
-//   );
-// }
